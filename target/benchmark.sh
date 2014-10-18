29a9221ea9ac198ca2bfc64e0c740caf83455a30
Dániel Stein <daniel@stein.hu>
2014-10-18 16:38:48 +0200

    Added benchmark script.

---
+++ target/benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for project in {physhun,djvu,xalan,cloudstack}; do
+
+	rm -rf ./toprocess/*
+	cp ~/mondo-codemodel/source-projects/$project/ ./toprocess/
+
+	mkdir -p ~/mondo-codemodel/results/$project/
+
+	for i in {1..5}; do
+		./transform-import-check.sh
+		cp ./results/sum.txt ~/mondo-codemodel/results/$project/sum$i.txt
+		echo $project $i done
+	done
+
+	mkdir -p ~/mondo-codemodel/exports/$project/
+	cp ./export/ ~/mondo-codemodel/exports$/project/
+done
--- target/import-check.sh
+++ target/import-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # clean the database
-./4store-graph-driver/scripts/4s-boss-start-single.sh 
-./4store-graph-driver/scripts/4s-restart.sh
+~/4store-graph-driver/scripts/4s-boss-start-single.sh 
+~/4store-graph-driver/scripts/4s-restart.sh
 
 FILES=$(find `pwd`/export/ -name "*.ttl")
 
@@ -10,7 +10,7 @@ FILES=$(find `pwd`/export/ -name "*.ttl")
 STIME=$(($(date +%s%N)/1000000))
 for f in $FILES; do
 #	STARTTIME=$(($(date +%s%N)/1000000))
-	./4store-graph-driver/scripts/4s-import.sh $f
+	~/4store-graph-driver/scripts/4s-import.sh $f
 #	let STARTTIME=$(($(date +%s%N)/1000000))-$STARTTIME
 #	echo $f, $STARTTIME
 done
@@ -19,37 +19,37 @@ let STIME=$(($(date +%s%N)/1000000))-$STIME
 echo import time: $STIME >> results/sum.txt
 
 STIME=$(($(date +%s%N)/1000000))
-./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-a.sparql`" | tail -n +2 | tee results/results-a.txt
+~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-a.sparql`" | tail -n +2 | tee results/results-a.txt
 let STIME=$(($(date +%s%N)/1000000))-$STIME
 echo a runtime: $STIME >> results/sum.txt
 cat results/results-a.txt | wc -l >> results/results-a.txt
 
 STIME=$(($(date +%s%N)/1000000))
-./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-b.sparql`" | tail -n +2 | tee results/results-b.txt
+~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-b.sparql`" | tail -n +2 | tee results/results-b.txt
 let STIME=$(($(date +%s%N)/1000000))-$STIME
 echo b runtime: $STIME >> results/sum.txt
 cat results/results-b.txt | wc -l >> results/results-b.txt
 
 STIME=$(($(date +%s%N)/1000000))
-./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-c.sparql`" | tail -n +2 | tee results/results-c.txt
+~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-c.sparql`" | tail -n +2 | tee results/results-c.txt
 let STIME=$(($(date +%s%N)/1000000))-$STIME
 echo c runtime: $STIME >> results/sum.txt
 cat results/results-c.txt | wc -l >> results/results-c.txt
 
 STIME=$(($(date +%s%N)/1000000))
-./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-d.sparql`" | tail -n +2 | tee results/results-d.txt
+~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-d.sparql`" | tail -n +2 | tee results/results-d.txt
 let STIME=$(($(date +%s%N)/1000000))-$STIME
 echo d runtime: $STIME >> results/sum.txt
 cat results/results-d.txt | wc -l >> results/results-d.txt
 
 #STIME=$(($(date +%s%N)/1000000))
-#./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-e.sparql`" | tail -n +2 | tee results/results-e.txt
+#~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-e.sparql`" | tail -n +2 | tee results/results-e.txt
 #let STIME=$(($(date +%s%N)/1000000))-$STIME
 #echo e runtime: $STIME 
 #cat results/results-e.txt | wc -l >> results/results-e.txt
 
 #STIME=$(($(date +%s%N)/1000000))
-#./4store-graph-driver/scripts/4s-query.sh  "`cat ../../tdk-codemodel/src/sources/csmr-f.sparql`" | tail -n +2 | tee results/results-f.txt
+#~/4store-graph-driver/scripts/4s-query.sh  "`cat ../queries/csmr-f.sparql`" | tail -n +2 | tee results/results-f.txt
 #let STIME=$(($(date +%s%N)/1000000))-$STIME
 #echo f runtime: $STIME 
 #cat results/results-f.txt | wc -l >> results/results-f.txt
